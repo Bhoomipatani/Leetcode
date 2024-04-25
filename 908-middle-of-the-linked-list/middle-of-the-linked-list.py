7d99d586eca996af1e2552f30cd1cd4b9e03c4f6
Bhoomipatani <58582545+Bhoomipatani@users.noreply.github.com>
2024-04-25 22:51:39 +0530

    Time: 42 ms (11.06%) | Memory: 16.5 MB (85.23%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.py
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -7,7 +7,7 @@ class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         slow=head
         fast=head
-        while fast and fast.next and slow:
+        while fast and fast.next:
             fast=fast.next.next
             slow=slow.next
         return slow
