3899dcdfad94453f93b483693c97125d1a32ba5f
Bhoomipatani <58582545+Bhoomipatani@users.noreply.github.com>
2024-06-10 11:32:21 +0530

    Time: 46 ms (59.30%) | Memory: 16.5 MB (57.58%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,10 +1,13 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
         sys_val={'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+        i=len(s)-2
         ans=0
-        s = s.replace("IV", "IIII").replace("IX", "VIIII")
-        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
-        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
-        for i in s:
-            ans+=sys_val[i]
-        return ans
+        while i>=0:
+            if sys_val[s[i]]<sys_val[s[i+1]]:
+                ans-=sys_val[s[i]]
+            else:
+                ans+=sys_val[s[i]]
+            
+            i-=1
+        return ans+sys_val[s[-1]]
