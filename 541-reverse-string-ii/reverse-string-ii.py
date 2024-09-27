278a6993e4b8ea798b85e20bd08c93d3bfc09e43
Bhoomi Patani <58582545+Bhoomipatani@users.noreply.github.com>
2024-09-28 03:12:03 +0530

    Time: 44 ms (13.70%) | Memory: 16.7 MB (63.58%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.py
+++ 541-reverse-string-ii/reverse-string-ii.py
@@ -4,13 +4,7 @@ class Solution:
         if len(s)<=k:
             return s[::-1]
         while i <len(s):
-            # print(s,i)
-            if i==0:
-                s=s[i:i+k][::-1]+s[i+k::]
-            elif i%(2*k)==0 and i!=0:
-                # print(s[::i])
-                # print(s[i:i+k])
-                # print(s[i+k::])
+            if i%(2*k)==0 :
                 s=s[0:i]+s[i:i+k][::-1]+s[i+k::]
             i+=1
         return s
