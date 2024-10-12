895d6a4fc1c655839eaee7ae0299b00565d58209
Bhoomi Patani <58582545+Bhoomipatani@users.noreply.github.com>
2024-10-11 21:46:17 -0700

    Time: 32 ms (88.39%) | Memory: 16.8 MB (20.59%) - LeetSync

--- 686-repeated-string-match/repeated-string-match.py
+++ 686-repeated-string-match/repeated-string-match.py
@@ -1,9 +1,10 @@
 class Solution:
     def repeatedStringMatch(self, a: str, b: str) -> int:
         n, m = len(a), len(b)
-        l = (m - 1) // n + 1
-        for i in range(l, l + 3):
-            ra = a * i
-            if ra.find(b) >= 0:
-                return i
+        l = m//n
+        copya=""
+        for i in range(l+2):
+            copya+=a
+            if b in copya:
+                return i+1
         return -1
