12de2596433f28e3ee6903fb3ca2a5a4332ed2e2
Bhoomi Patani <58582545+Bhoomipatani@users.noreply.github.com>
2024-10-16 11:06:41 -0700

    Time: 71 ms (61.33%) | Memory: 16.9 MB (90.62%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -3,9 +3,6 @@ class Solution:
         curMax, curMin=1,1
         res=max(nums)
         for n in nums:
-            if n==0:
-                curMin, curMax=1,1
-                continue
             tmp=curMax
             curMax=max(n*curMax, n*curMin, n)
             curMin=min(tmp*n, n*curMin, n)
