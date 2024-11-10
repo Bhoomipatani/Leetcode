70274d6df3aa236f3d09f9feb086e0e98d971f6e
Bhoomi Patani <58582545+Bhoomipatani@users.noreply.github.com>
2024-11-10 08:58:44 -0800

    Time: 3 ms (12.04%) | Memory: 17.1 MB (9.36%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,17 +1,9 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        memo = {}
-
-        def dp(start_index, i):
-            if i == start_index:
-                return nums[start_index]
-            if i == start_index + 1:
-                return max(nums[start_index], nums[start_index + 1])
-
-            if (start_index, i) in memo:
-                return memo[(start_index, i)]
-
-            memo[(start_index, i)] = max(nums[i] + dp(start_index, i-2), dp(start_index, i-1))
-            return memo[(start_index, i)]
-
-        return nums[0] if len(nums) == 1 else max(dp(0, len(nums) - 2), dp(1, len(nums) - 1))
+        n = len(nums)
+        @cache
+        def dfs(i, robbedFirst):
+            if (i >= n or (i == n - 1 and robbedFirst)):
+                return 0            
+            return max(dfs(i + 1, robbedFirst), nums[i] + dfs(i + 2, robbedFirst))
+        return max(dfs(1, False), nums[0] + dfs(2, True))
