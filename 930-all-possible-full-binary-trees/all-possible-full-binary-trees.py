2a85746ca9c0aac5fec89dc4de72431cfad7562d
Bhoomi Patani <58582545+Bhoomipatani@users.noreply.github.com>
2024-11-12 19:08:05 -0800

    Time: 0 ms (100.00%) | Memory: 20.6 MB (52.48%) - LeetSync

---
+++ 930-all-possible-full-binary-trees/all-possible-full-binary-trees.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def allPossibleFBT(self, n: int) -> List[Optional[TreeNode]]:
+        @lru_cache(None)
+        def recurse(count):
+            if count == 1: return [TreeNode()]
+            output = []
+            for i in range((count - 1) // 2):
+                l_output, r_output = recurse(i * 2 + 1), recurse(count - 2 - i * 2)
+                for j in range(len(l_output)):
+                    for k in range(len(r_output)):
+                        output.append(TreeNode(val=0, left=l_output[j], right=r_output[k]))
+            return output
+        return recurse(n)
+        
