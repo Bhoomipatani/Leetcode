14209df0b72b5ec491f0a6b5a58ee2152677d27e
Bhoomi Patani <58582545+Bhoomipatani@users.noreply.github.com>
2024-11-23 11:14:17 -0800

    Time: 3 ms (88.57%) | Memory: 17.3 MB (99.75%) - LeetSync

--- 724-find-pivot-index/find-pivot-index.py
+++ 724-find-pivot-index/find-pivot-index.py
@@ -1,12 +1,9 @@
-class Solution:
-    def pivotIndex(self, nums: List[int]) -> int:
-        larr=[0]*len(nums)
-        rarr=[0]*len(nums)
-        n=len(nums)
-        for i in range(1,len(nums)):
-            larr[i]=larr[i-1]+nums[i-1]
-            rarr[n-i-1]=rarr[n-i]+nums[n-i]
-        for i in range(0,len(nums)):
-            if larr[i]==rarr[i]:
+class Solution(object):
+    def pivotIndex(self, nums):
+        S = sum(nums)
+        leftsum = 0
+        for i, x in enumerate(nums):
+            if leftsum == (S - leftsum - x):
                 return i
+            leftsum += x
         return -1
