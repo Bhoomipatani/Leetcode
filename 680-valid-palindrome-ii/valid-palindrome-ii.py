56c15aeef477b90bbfc5153a406570f6ed4e4eb0
Bhoomi Patani <58582545+Bhoomipatani@users.noreply.github.com>
2024-11-29 20:21:41 -0800

    Time: 61 ms (39.91%) | Memory: 17.6 MB (10.00%) - LeetSync

--- 680-valid-palindrome-ii/valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/valid-palindrome-ii.py
@@ -1,40 +1,21 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        l=0
-        delete=0
-        ans1=ans2=True
-        r=len(s)-1
-        while l<=r:
-            # print(l,r,s[l],s[r])
-            if s[l]==s[r]:
-                l+=1
-                r-=1
-            elif l+1<=r and s[l+1]==s[r]:
-                l+=1
-                delete+=1
-                if delete>1:
-                    ans1= False
-                    break
-            else:
-                ans1= False
-                break
-        l=0
-        delete=0
-        r=len(s)-1
-        while l<=r:
-            print(l,r,s[l],s[r])
-            if s[l]==s[r]:
-                l+=1
-                r-=1
-            elif r-1>=l and s[r-1]==s[l]:
-                r-=1
-                delete+=1
-                if delete>1:
-                    ans2= False
-                    break
-            else:
-                ans2= False
-                break
-        if ans1 or ans2:    
+        def check_palindrome(s, i, j):
+            while i < j:
+                if s[i] != s[j]:
+                    return False
+                i += 1
+                j -= 1
+            
             return True
-        return False
+
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            # Found a mismatched pair - try both deletions
+            if s[i] != s[j]:
+                return check_palindrome(s, i, j - 1) or check_palindrome(s, i + 1, j)
+            i += 1
+            j -= 1
+        
+        return True
