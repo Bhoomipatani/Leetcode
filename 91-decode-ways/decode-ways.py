e1a05d1785e717ed15deb8df93befdc295a4db16
Bhoomi Patani <58582545+Bhoomipatani@users.noreply.github.com>
2024-12-24 13:13:14 -0800

    Time: 0 ms (100.00%) | Memory: 17.9 MB (5.11%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -6,7 +6,6 @@ class Solution:
         dp[1] = 0 if s[0] == "0" else 1
 
         for i in range(2, len(dp)):
-            print(dp)
             if s[i - 1] != "0":
                 dp[i] = dp[i - 1]
 
