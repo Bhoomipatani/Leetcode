e6738eaf0da623b5ff8933f4587ed728f2ffa131
Bhoomi Patani <58582545+Bhoomipatani@users.noreply.github.com>
2025-01-10 11:19:12 -0800

    Time: 4 ms (70.12%) | Memory: 18.9 MB (50.71%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -3,22 +3,23 @@ class Solution:
         indegree = [0] * numCourses
         adj = [[] for _ in range(numCourses)]
 
-        for prerequisite in prerequisites:
-            adj[prerequisite[1]].append(prerequisite[0])
-            indegree[prerequisite[0]] += 1
-        queue = deque()
+        for pre in prerequisites:
+            adj[pre[1]].append(pre[0])
+            indegree[pre[0]]+=1
+            # print(indegree,adj)
+        q=deque()
         for i in range(numCourses):
-            if indegree[i] == 0:
-                queue.append(i)
+            if indegree[i]==0:
+                q.append(i)
 
-        nodesVisited = 0
-        while queue:
-            node = queue.popleft()
-            nodesVisited += 1
+        visited=0
+        while q:
+            node=q.popleft()
+            visited+=1
+            for nei in adj[node]:
+                indegree[nei]-=1
+                if indegree[nei]==0:
+                    q.append(nei)
+        # print(visited)
+        return visited==numCourses
 
-            for neighbor in adj[node]:
-                indegree[neighbor] -= 1
-                if indegree[neighbor] == 0:
-                    queue.append(neighbor)
-
-        return nodesVisited == numCourses
