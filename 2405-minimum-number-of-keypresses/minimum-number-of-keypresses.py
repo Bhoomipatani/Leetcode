f70436ccc0ca7c951504be7460473a55c0c9b64d
Bhoomi Patani <58582545+Bhoomipatani@users.noreply.github.com>
2025-01-13 14:14:33 -0800

    Time: 47 ms (5.22%) | Memory: 18.6 MB (8.86%) - LeetSync

---
+++ 2405-minimum-number-of-keypresses/minimum-number-of-keypresses.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minimumKeypresses(self, s: str) -> int:
+        n=len(s)
+        hm={}
+        for i in range(n):
+            if s[i] not in hm:
+                hm[s[i]]=0
+            hm[s[i]]+=1
+        hm = dict(sorted(hm.items(), key=lambda item: item[1], reverse=True))
+        press=0
+        print(hm)
+        l=0
+        for v in hm.values():
+            if l<9:
+                press+=v
+                print(press)
+            elif 9<=l<18:
+                press+=2*v
+            else:
+                press+=3*v
+            l+=1
+        return press
