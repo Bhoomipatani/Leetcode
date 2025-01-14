e2885a1b3bcbdb3893fe471a9d33bc38f61503e9
Bhoomi Patani <58582545+Bhoomipatani@users.noreply.github.com>
2025-01-13 16:48:08 -0800

    Time: 9 ms (41.95%) | Memory: 18.5 MB (6.94%) - LeetSync

---
+++ 1829-maximum-units-on-a-truck/maximum-units-on-a-truck.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maximumUnits(self, boxType: List[List[int]], trucksize: int) -> int:
+        # boxType=sorted(boxType, key=lambda x:x[0],reverse=True)
+        boxType=sorted(boxType, key=lambda x:x[1],reverse=True)
+        totalunit=0
+        print(boxType)
+        for i in range(len(boxType)):
+            print(trucksize)
+            if trucksize==0:
+                break
+            elif trucksize-boxType[i][0]>0:
+                # print(boxType[i])
+                totalunit+=boxType[i][0]*boxType[i][1]
+                trucksize-=boxType[i][0]
+            elif trucksize-boxType[i][0]<=0 :
+                totalunit+=trucksize*boxType[i][1]
+                trucksize=0
+        return totalunit
