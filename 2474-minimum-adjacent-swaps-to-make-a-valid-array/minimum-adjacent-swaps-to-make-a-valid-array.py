3ce3ba5d3d58beaedb7df51a88cad1c3e8e3624c
Bhoomi Patani <58582545+Bhoomipatani@users.noreply.github.com>
2025-04-15 12:29:35 -0700

    Time: 30 ms (14.10%) | Memory: 30.9 MB (63.84%) - LeetSync

---
+++ 2474-minimum-adjacent-swaps-to-make-a-valid-array/minimum-adjacent-swaps-to-make-a-valid-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def minimumSwaps(self, nums: List[int]) -> int:
+        n=len(nums)
+        swaps=0
+        minnum=min(nums)
+        maxnum=max(nums)
+        minindex,maxindex=float('inf'), float('-inf')
+        for i in range(n):
+            if nums[i]==minnum:
+                minindex=min(minindex,i)
+            if nums[i]==maxnum:
+                maxindex=max(maxindex,i)
+        print(minindex,maxindex)
+        if minindex>maxindex:
+            swap=minindex+n-maxindex-2
+        else:
+            swap=minindex+n-maxindex-1
+        return swap
